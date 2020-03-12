9e02f36ba472d011165243a479c23c130d9dec26
akaset <esaka.tom@gmail.com>
2020-03-12 23:22:27 +0900

    more simple

--- merge_two_binary_trees.go
+++ merge_two_binary_trees.go
@@ -2,27 +2,16 @@ package leetcode_golang
 
 // https://leetcode.com/problems/merge-two-binary-trees/
 func mergeTrees(t1 *TreeNode, t2 *TreeNode) *TreeNode {
-	t1 = merge(t1, t2)
-	return t1
-}
-
-func merge(t1 *TreeNode, t2 *TreeNode) *TreeNode {
 	if t2 == nil {
 		return t1
 	}
 	if t1 == nil {
-		t1 = &TreeNode{}
+		return t2
 	}
 
 	t1.Val += t2.Val
 
-	if t1.Left == nil && t2.Left != nil {
-		t1.Left = &TreeNode{}
-	}
-	if t1.Right == nil && t2.Right != nil {
-		t1.Right = &TreeNode{}
-	}
-	merge(t1.Left, t2.Left)
-	merge(t1.Right, t2.Right)
+	t1.Left = mergeTrees(t1.Left, t2.Left)
+	t1.Right = mergeTrees(t1.Right, t2.Right)
 	return t1
 }
